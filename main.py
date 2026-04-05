544edfd90f9d1ca82aef4e3b81f8395cf2349c08
dissolutt <pnv2306@gmail.com>
2026-04-06 01:40:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -283,41 +283,41 @@ def handle_teachers(message):
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "dancehall")
 def handle_dancehall(message):
-    send_video_smart(message.chat.id, "dancehall.mp4", "направление: dancehall\nтренер: катя\nрасписание: пн, ср 18:00")
+    send_video_smart(message.chat.id, "dancehall.mp4", "направление: dancehall\nтренер: Катя\nрасписание: пн, ср 18:00")
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_dancehall_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "hip-hop")
 def handle_hiphop(message):
-    text = "направление: hip-hop\nтренер: никита\nрасписание: вт, чт 20:00 (15+)\nвт, чт 18:00 (6-9 лет)\nпн 19:00, вт 20:00 (9-14 лет)"
+    text = "направление: hip-hop\nтренер: Никита\nрасписание: вт, чт 20:00 (15+)\nвт, чт 18:00 (6-9 лет)\nпн 19:00, вт 20:00 (9-14 лет)"
     send_video_smart(message.chat.id, "hiphop.mp4", text)
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_hiphop_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "contemporary")
 def handle_contemporary(message):
-    text = "направление: contemporary\nтренер: даша\nрасписание: вт, чт 19:00 (9-13 лет)\nвт, чт 20:00 (17+)"
+    text = "направление: contemporary\nтренер: Даша\nрасписание: вт, чт 19:00 (9-13 лет)\nвт, чт 20:00 (17+)"
     send_video_smart(message.chat.id, "contemporary.mp4", text)
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_contemporary_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "jazz modern")
 def handle_jazzmodern(message):
-    send_video_smart(message.chat.id, "jazzmodern.mp4", "направление: jazz modern\nтренер: даша\nрасписание: пн, ср 19:00 (17+)")
+    send_video_smart(message.chat.id, "jazzmodern.mp4", "направление: jazz modern\nтренер: Даша\nрасписание: пн, ср 19:00 (17+)")
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_jazzmodern_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "jazz-funk")
 def handle_jazzfunk(message):
-    text = "направление: jazz-funk\nтренер: настя\nрасписание: пн, ср 18:00/19:00 (набор закрыт)\nпн, пт 17:00/19:00 (набор закрыт)\nср, пт 18:00 (11+)"
+    text = "направление: jazz-funk\nтренер: Настя\nрасписание: пн, ср 18:00/19:00 (набор закрыт)\nпн, пт 17:00/19:00 (набор закрыт)\nср, пт 18:00 (11+)"
     send_video_smart(message.chat.id, "jazzfunk.mp4", text)
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_jazzfunk_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "k-pop")
 def handle_kpop(message):
-    text = "направление: k-pop\nтренер: алёна\nрасписание: сб, вс 12:00 (9-16 лет)\nсб, вс 14:00 (10-16 лет)\nсб, вс 13:00 (группа для родителей 20+)"
+    text = "направление: k-pop\nтренер: Алёна\nрасписание: сб, вс 12:00 (9-16 лет)\nсб, вс 14:00 (10-16 лет)\nсб, вс 13:00 (группа для родителей 20+)"
     send_video_smart(message.chat.id, "kpop.mp4", text)
     bot.send_message(message.chat.id, "выберите направление", reply_markup=after_kpop_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "расписание")
 def handle_schedule(message):
-    bot.send_message(message.chat.id, "📅 Расписание у администратора", reply_markup=main_menu())
+    bot.send_message(message.chat.id, "необходима предварительная запись на занятия!", reply_markup=main_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "прайс")
 def handle_price(message):
@@ -325,32 +325,31 @@ def handle_price(message):
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "аренда залов")
 def handle_rental(message):
-    send_video_smart(message.chat.id, "rental.mp4", RENTAL_TEXT)
-    bot.send_message(message.chat.id, "выбери раздел 👇", reply_markup=main_menu())
+    send_video_smart(message.chat.id, "rental.mp4", RENTAL_TEXT), reply_markup=main_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "абонементы")
 def handle_subscriptions(message):
-    bot.send_message(message.chat.id, "у вас нет активных абонементов", reply_markup=subscriptions_menu())
+    bot.send_message(message.chat.id, "у Вас нет активных абонементов", reply_markup=subscriptions_menu())
 
 @bot.message_handler(func=lambda m: m.text and m.text.lower() == "продлить прошлый абонемент")
 def handle_renew(message):
-    bot.send_message(message.chat.id, "свяжитесь с нами: +7 951 772 98 98", reply_markup=subscriptions_menu())
+    bot.send_message(message.chat.id, "для продления абонемента свяжитесь с нами: +7 951 772 98 98\n e-motion74@bk.ru", reply_markup=subscriptions_menu())
 
-@bot.message_handler(func=lambda m: m.text and m.text.lower() in ["приобрести новый абонемент", "разовое занятие", "пробное занятие"])
-def handle_buy(message):
-    bot.send_message(message.chat.id, "выберите направление", reply_markup=buy_directions_menu())
+@bot.message_handler(func=lambda m: m.text and m.text.lower() == "приобрести новый абонемент")
+def handle_renew(message):
+    bot.send_message(message.chat.id, "для приобретения абонемента свяжитесь с нами: +7 951 772 98 98\n e-motion74@bk.ru", reply_markup=subscriptions_menu())
 
-@bot.message_handler(func=lambda m: m.text.lower() == "воздушные 🛒")
-def handle_aerial_buy(message):
-    bot.send_message(message.chat.id, AERIAL_TEXT, reply_markup=buy_aerial_menu())
+@bot.message_handler(func=lambda m: m.text and m.text.lower() == "разовое занятие")
+def handle_renew(message):
+    bot.send_message(message.chat.id, "для оплаты разового занятия свяжитесь с нами: +7 951 772 98 98\n e-motion74@bk.ru", reply_markup=subscriptions_menu())
 
-@bot.message_handler(func=lambda m: m.text.lower() == "танцевальные 🛒")
-def handle_dance_buy(message):
-    bot.send_message(message.chat.id, DANCE_TEXT, reply_markup=buy_dance_menu())
+@bot.message_handler(func=lambda m: m.text and m.text.lower() == "пробное занятие")
+def handle_renew(message):
+    bot.send_message(message.chat.id, "для записи на пробное занятие свяжитесь с нами: +7 951 772 98 98\n e-motion74@bk.ru", reply_markup=subscriptions_menu())
 
 @bot.message_handler(func=lambda m: m.text == "◀️ к абонементам")
 def handle_back_to_subscriptions(message):
-    bot.send_message(message.chat.id, "у вас нет активных абонементов", reply_markup=subscriptions_menu())
+    bot.send_message(message.chat.id, "у Вас нет активных абонементов", reply_markup=subscriptions_menu())
 
 @bot.message_handler(content_types=["video"])
 def handle_video_message(message):
